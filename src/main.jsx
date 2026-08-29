a621ffa23fb96d4cbee58c09933f34b3690608e0
Kevin Vaca <77655154+ClasK7@users.noreply.github.com>
2026-08-29 12:34:41 -0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@coreui/coreui/dist/css/coreui.min.css' // <-- Estilos de la plantilla CoreUI
 import './styles.css'
 import App from './App.jsx'
 
